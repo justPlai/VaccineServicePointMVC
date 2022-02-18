a14d6c8f06b2d3f03ba080bd4658b66484b7dd93
NuttawatJuntawong <91516482+NuttawatJuntawong@users.noreply.github.com>
2022-02-18 19:59:59 +0700

    Update connection_connect.php

--- connection_connect.php
+++ connection_connect.php
@@ -3,7 +3,7 @@
     $severname = "localhost";
     $username = "root";
     $password ="";
-    $dbname = "vaccineservicepoint";
+    $dbname = "vaccine";
     $conn = mysqli_connect($severname, $username, $password);
     mysqli_set_charset($conn, "utf8");
     if($conn->connect_error){
