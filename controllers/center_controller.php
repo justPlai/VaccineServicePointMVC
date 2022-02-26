8f1ac05a35ea3aa5b53265e54cb5a699f097c59b
ArtidYenpram <98255814+justPlai@users.noreply.github.com>
2022-02-27 01:07:51 +0700

    format document

--- controllers/center_controller.php
+++ controllers/center_controller.php
@@ -1,80 +1,84 @@
 <?php
-    class CentersController{
-        public function search(){
-            $key = $_GET['key'];
-            $centerList = center::search($key);
-            require_once('views/center/index.php');
-        }
-        public function searchupdateCenter(){
-            $key = $_GET['key'];
-            echo $key;
-            $centerList = center::search($key);
-            require_once('views/center/updateCenterIndex.php');
-        }
-
-        public function index(){
-            $key = "";
-            $centerList = Center::getAll();
-            require_once('views/center/index.php');
-        }
-
-        public function updateCenterIndex(){
-            $centerList = Center::getAll();
-            require_once('views/center/updateCenterIndex.php');
-        }
-
-        public function updateFormCenter(){
-            $id = $_GET['id'];
-            $center = Center::get($id);
-            require_once('views/center/updateCenterPage.php');
-        }
-
-        public function updateCenter(){
-            $id = $_GET['id'];
-            $CenterName = $_GET['CenterName'];
-            $DateStart = $_GET['DateStart'];
-            $DateStop = $_GET['DateStop'];
-            $TimeStart = $_GET['TimeStart'];
-            $TimeStop = $_GET['TimeStop'];
-            $Websitelink = $_GET['Websitelink'];
-            $Imagelink = $_GET['Imagelink'];
-            $Locationlink = $_GET['Locationlink'];
-            //echo $id." ".$CenterName." ".$DateStart." ".$DateStop." ".$TimeStart." ".$TimeStop." ".$Websitelink." ".$Imagelink." ".$Locationlink;
-            Center::update($id,$CenterName,$DateStart,$DateStop,$TimeStart,$TimeStop,$Websitelink,$Imagelink,$Locationlink);
-            CentersController::updateCenterIndex();
-        }
-
-
-        public function addCenterPage(){
-            require_once('views/center/addCenterPage.php');
-        }
-
-        public function addCenter()
-        {
-            //echo " in addCenter";
-            $CenterName = $_GET['CenterName'];
-            $DateStart = $_GET['DateStart'];
-            $DateStop = $_GET['DateStop'];
-            $TimeStart = $_GET['TimeStart'];
-            $TimeStop = $_GET['TimeStop'];
-            $Websitelink = $_GET['Websitelink'];
-            $Imagelink = $_GET['Imagelink'];
-            $Locationlink = $_GET['Locationlink'];
-            $filler = $_GET['filler'];
-            
-            //echo $CenterName." ".$DateStart." ".$DateStop." ".$TimeStart." ".$TimeStop." ".$Websitelink." ".$Imagelink." ".$Locationlink." ".$filler;
-
-            Center::Add($CenterName,$DateStart,$DateStop,$TimeStart,$TimeStop,$Websitelink,$Imagelink,$Locationlink,$filler);
-            CentersController::index();
-            
-
-        }
-
-        public function delete(){
-            $id = $_GET['id'];
-            Center::delete($id);
-            CentersController::updateCenterIndex();
-        }
-
+class CentersController
+{
+    public function search()
+    {
+        $key = $_GET['key'];
+        $centerList = center::search($key);
+        require_once('views/center/index.php');
     }
-?>
+    public function searchupdateCenter()
+    {
+        $key = $_GET['key'];
+        echo $key;
+        $centerList = center::search($key);
+        require_once('views/center/updateCenterIndex.php');
+    }
+
+    public function index()
+    {
+        $key = "";
+        $centerList = Center::getAll();
+        require_once('views/center/index.php');
+    }
+
+    public function updateCenterIndex()
+    {
+        $centerList = Center::getAll();
+        require_once('views/center/updateCenterIndex.php');
+    }
+
+    public function updateFormCenter()
+    {
+        $id = $_GET['id'];
+        $center = Center::get($id);
+        require_once('views/center/updateCenterPage.php');
+    }
+
+    public function updateCenter()
+    {
+        $id = $_GET['id'];
+        $CenterName = $_GET['CenterName'];
+        $DateStart = $_GET['DateStart'];
+        $DateStop = $_GET['DateStop'];
+        $TimeStart = $_GET['TimeStart'];
+        $TimeStop = $_GET['TimeStop'];
+        $Websitelink = $_GET['Websitelink'];
+        $Imagelink = $_GET['Imagelink'];
+        $Locationlink = $_GET['Locationlink'];
+        Center::update($id, $CenterName, $DateStart, $DateStop, $TimeStart, $TimeStop, $Websitelink, $Imagelink, $Locationlink);
+        CentersController::updateCenterIndex();
+    }
+
+
+    public function addCenterPage()
+    {
+        require_once('views/center/addCenterPage.php');
+    }
+
+    public function addCenter()
+    {
+        //echo " in addCenter";
+        $CenterName = $_GET['CenterName'];
+        $DateStart = $_GET['DateStart'];
+        $DateStop = $_GET['DateStop'];
+        $TimeStart = $_GET['TimeStart'];
+        $TimeStop = $_GET['TimeStop'];
+        $Websitelink = $_GET['Websitelink'];
+        $Imagelink = $_GET['Imagelink'];
+        $Locationlink = $_GET['Locationlink'];
+        $filler = $_GET['filler'];
+
+        //echo $CenterName." ".$DateStart." ".$DateStop." ".$TimeStart." ".$TimeStop." ".$Websitelink." ".$Imagelink." ".$Locationlink." ".$filler;
+
+        Center::Add($CenterName, $DateStart, $DateStop, $TimeStart, $TimeStop, $Websitelink, $Imagelink, $Locationlink, $filler);
+        CentersController::index();
+    }
+
+    public function delete()
+    {
+        $id = $_GET['id'];
+        Center::delete($id);
+        CentersController::updateCenterIndex();
+    }
+}
--- controllers/vaccine_controller.php
+++ controllers/vaccine_controller.php
@@ -37,4 +37,3 @@
             VaccinesController::index();
         }
     }
-?>
