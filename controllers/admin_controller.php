616d9ae89ce6827a4daba6a39c016e867cd4be53
ArtidYenpram <98255814+justPlai@users.noreply.github.com>
2022-02-27 18:39:54 +0700

    Update admin_controller.php

--- controllers/admin_controller.php
+++ controllers/admin_controller.php
@@ -4,21 +4,14 @@
             require_once('views/admin/index.php');
         }
 
-        public function goHome(){
-            require_once('views/page/home.php');
-        }
-
         public function signIn(){
             $baseUsername = $_GET['username'];
             $basePassword = $_GET['password'];
             
             $account = Account::signIn($baseUsername, $basePassword);
-            echo "<- token=$account->token ->";
             $token = $account->token;
             $_SESSION['username'] = $account->user;
             $_SESSION['token'] = "1";
-            echo $_SESSION['username'];
-            echo $_SESSION['token'];
             header("Location: index.php");
             
         }
